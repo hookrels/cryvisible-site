569040b3a7de67aa8788c8c130f5173e14cdf12e
hookrels <hopoku775@gmail.com>
2025-09-22 13:27:48 +0100

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,9 @@
+export default function HomePage() {
+  return (
+    <main style={{ padding: 24 }}>
+      <h1>Cryvisible</h1>
+      <p>This is the public Landing Page.</p>
+      <p><a href="/app">Go to app (will ask you to sign in)</a></p>
+    </main>
+  )
+}
