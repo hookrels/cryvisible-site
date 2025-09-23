c339dd103807ca5dc74474136d35653ef2643d54
hookrels <hopoku775@gmail.com>
2025-09-23 10:46:58 +0100

    Delete app directory

--- app/page.js
+++
@@ -1,9 +0,0 @@
-export default function HomePage() {
-  return (
-    <main style={{ padding: 24 }}>
-      <h1>Cryvisible</h1>
-      <p>This is the public Landing Page.</p>
-      <p><a href="/app">Go to app (will ask you to sign in)</a></p>
-    </main>
-  )
-}
